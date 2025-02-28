1a7a7f5e96c0ffd12fc1a316e0d5437d55c15f42
Alan Gaurav <alangaurav@gmail.com>
2025-02-28 10:40:45 +0000

    Regression: Refactor code.
    LinearRegression class cleaned up, addition of
    helper functions for abstractions.
    
    Signed-off-by: Alan Gaurav <alangaurav@gmail.com>

--- regression.py
+++ regression.py
@@ -2,37 +2,14 @@ from matrix import Matrix
 from metrics import Metrics
 import random
 
+
 # Multivariate Linear Regression, can be expanded to polynomial.
 class LinearRegression:
-    def __init__(self, degree = 1):
+
+    def __init__(self, degree=1):
         self.degree = degree
 
-    def fit(self, x_train, y_train):
-        if len(y_train) != len(x_train):
-            raise TypeError("Invalid dimensions of X and Y data.")
-
-        y = Matrix(len(y_train), 1, y_train)
-        data = []
-        for i in range(len(x_train)):
-            row = x_train[i]
-            if type(x_train[i]) is int:
-                x_train[i] = [1, x_train[i]]
-            else:
-                row.insert(0, 1)
-            data.extend(x_train[i])
-
-        x = Matrix(len(x_train), len(x_train[0]), data)
-        x_t = x.transpose()
-        # Change this to the summation fn. matrix for XtX * XtY => summation of xi ** i matrix * summation of yi ** i matrix
-        # IMPORTANT: If there is a linear relationship between xi then this method is not possible.
-        # i.e. Rank(X) >= m (if n x m matrix)
-        xtx = x_t.multiply(x)
-        xtx_inv = xtx.inverse()
-        xty = x_t.multiply(y)
-        self.theta = xtx_inv.multiply(xty)
-        self.x = x
-
-    def predict(self, x, theta = None):
+    def gen_x_matrix(self, x):
         data = []
         for i in range(len(x)):
             row = x[i]
@@ -41,9 +18,31 @@ class LinearRegression:
             else:
                 row.insert(0, 1)
             data.extend(x[i])
+        return Matrix(len(x), len(x[0]), data)
 
-        x = Matrix(len(x), len(x[0]), data)
+    def gen_y_matrix(self, y):
+        return Matrix(len(y), 1, y)
 
+    def normal_form(self, x, y):
+        x_t = x.transpose()
+        # TODO: Change this to the summation fn. matrix for XtX * XtY => summation of xi ** i matrix * summation of yi ** i matrix
+        # IMPORTANT: If there is a linear relationship between xi then this method is not possible.
+        # i.e. Rank(X) >= m (if n x m matrix)
+        xtx = x_t.multiply(x)
+        xtx_inv = xtx.inverse()
+        xty = x_t.multiply(y)
+        self.theta = xtx_inv.multiply(xty)
+
+    def fit(self, x_train, y_train):
+        if len(y_train) != len(x_train):
+            raise TypeError("Invalid dimensions of X and Y data.")
+        x = self.gen_x_matrix(x_train)
+        y = self.gen_y_matrix(y_train)
+        self.normal_form(x, y)
+
+    def predict(self, x, theta=None):
+        if type(x) is list:
+            x = self.gen_x_matrix(x)
         if theta is None:
             theta = self.theta
         preds = x.multiply(theta)
@@ -51,24 +50,24 @@ class LinearRegression:
 
     # Gradient descent based on loss fn: MSE
     # Gradient descent should be based on known y values.
-    # How do you achieve convergence??
-    def gradient_descent(self, y):
+    # TODO: Fix convergence condition
+    def gradient_descent(self, x, y):
         x_t = self.x.transpose()
-        theta = self.theta
-        y_preds_new = self.predict(self.x)
-        y_preds_old = Matrix(len(y_preds_new.matrix), 1, y)
+        theta = Matrix(
+            len(y), len(y[0]), [random.randint(-10, 10) for _ in range(len(y))]
+        )
+        y_preds_new = self.predict(x)
+        y_preds_old = y
         i = 0
         # Convergence is reached when prediction values do not change
         while i < 20 and not y_preds_new.equal(y_preds_old):
             y_dif = y_preds_new.subtract(y_preds_old)
             xtydif = x_t.multiply(y_dif)
-            learn_rate = xtydif.scalarMultiply((2) / len(self.theta.matrix))
-            theta = theta.subtract(learn_rate)
-            print(theta.matrix)
+            delta = xtydif.scalarMultiply((2) / len(self.theta.matrix))
+            theta = theta.subtract(delta)
             y_preds_old = y_preds_new
             y_preds_new = self.predict(self.x, theta)
             i += 1
-
         self.theta = theta
 
 
