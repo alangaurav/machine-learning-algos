dd0c6dd9fae3987f0a97a7abc9738849a4367858
Alan Gaurav <alangaurav@gmail.com>
2025-03-04 11:25:39 +0000

    Metrics: Added Mean Absolute Error.
    MAE now provides the mean absolute error as a
    regression metric.
    
    Signed-off-by: Alan Gaurav <alangaurav@gmail.com>

--- metrics.py
+++ metrics.py
@@ -6,3 +6,8 @@ class Metrics:
         N = len(y)
         mse = (sum([(y[i] - y_preds[i]) ** 2 for i in range(N)])) / N
         return mse
+
+    def mean_absolute_error(self, y, y_preds):
+        N = len(y)
+        mae = (sum([(y[i] - y_preds[i]) for i in range(N)])) / N
+        return mae
